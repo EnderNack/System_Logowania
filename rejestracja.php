0f342736ed57f87ae40a9db7b73aa74d523ad8ff
Kacper Wilczek <kwilczek107@gmail.com>
2021-03-19 10:57:11 +0100

    ciąg dalszy

--- rejestracja.php
+++ rejestracja.php
@@ -8,22 +8,26 @@
     <title>Panel rejestracji</title>
 </head>
 <body>
+        <div class="navbar">
+            
+        </div>
         <div class="panel">
-
-                <fieldset>
                 <form action="" method="post">
-                <input type="text" placeholder="Imię:" required>
-                <input type="text" placeholder="Nazwisko:" required>
-                <input type="text" placeholder="Login:" required>
-                <input type="password" placeholder="Hasło:" required>
-                <input type="password" placeholder="Powtórz hasło" required>
-                <input type="text" placeholder="Adres" required>
-                <input type="number" step=9 placeholder="Nr. tel:" required> 
-                <input type="email" placeholder="Email:" required> <br>
-                <input type="submit">  
-                 </form>
-                </fieldset> 
-           
+                    <h1>REJESTRACJA</h1>
+                    <input type="text" placeholder="Imię:" required>
+                    <input type="text" placeholder="Nazwisko:" required>
+                    <input type="text" placeholder="Login:" required>
+                    <input type="password" placeholder="Hasło:" required>
+                    <input type="password" placeholder="Powtórz hasło" required>
+                    <input type="text" placeholder="Adres" required>
+                    <input type="number" step=9 placeholder="Nr. tel:" required> 
+                    <input type="email" placeholder="Email:" required> <br>
+                    <input type="submit">  
+                </form>
+        </div>
+        <div class="stopka">
+       Piotr Pająk, Kacper Wilczek, Filip Urzoń, Łukasz Mieczkowski, Patryk Widomski, Weronika , System_Logowania
+
         </div>
 </body>
 </html>
